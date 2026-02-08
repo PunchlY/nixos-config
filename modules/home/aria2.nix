dae3f11d2619cdbf26ce5bf086d575d27b64c122
Punchly <punchly9lin@gmail.com>
2026-02-09 02:53:28 +0800

    feat: 添加 aria2 配置，更新网络主机列表

---
+++ modules/home/aria2.nix
@@ -0,0 +1,20 @@
+{
+  nixosConfig,
+  config,
+  lib,
+  ...
+}:
+let
+  escapeTmpfiles = lib.strings.escapeC [
+    "\t"
+    "\n"
+    "\r"
+    " "
+    "\\"
+  ];
+in
+{
+  systemd.user.tmpfiles.rules = lib.mkIf nixosConfig.services.aria2.enable [
+    "L ${escapeTmpfiles "${config.xdg.userDirs.download}/aria2"} - - - - ${escapeTmpfiles nixosConfig.services.aria2.settings.dir}"
+  ];
+}
