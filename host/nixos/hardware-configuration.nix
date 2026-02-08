39d2079a3d4f2beb76e772244aedbab6195719b8
Punchly <punchly9lin@gmail.com>
2026-02-08 12:22:49 +0800

    优化硬件配置，移除不必要的内核模块，简化可用内核模块列表

--- host/nixos/hardware-configuration.nix
+++ host/nixos/hardware-configuration.nix
@@ -14,10 +14,8 @@
   boot.initrd.availableKernelModules = [
     "nvme"
     "xhci_pci"
-    "thunderbolt"
+    "ahci"
     "usbhid"
-    "usb_storage"
-    "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
