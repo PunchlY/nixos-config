dd513904aabf771b8608989f81c04f7e769bfce2
PunchlY <punchly9lin@gmail.com>
2026-03-24 08:20:36 +0800

    refactor: remove wechat overlay and add wayllpaper/wleird packages

---
+++ packages/wayllpaper.nix
@@ -0,0 +1,50 @@
+{
+  stdenv,
+  lib,
+  fetchgit,
+  pkg-config,
+  meson,
+  ninja,
+  wayland,
+  wayland-scanner,
+  wayland-protocols,
+  cairo,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wayllpaper";
+  version = "0.1.0";
+
+  src = fetchgit {
+    url = "https://git.sr.ht/~kennylevinsen/wayllpaper";
+    rev = "cc60f668b584e61591fb99dc101353f16c0e1fcc";
+    hash = "sha256-g7yAaJsQpCVKBzRbVhKt7gPjb3y7k3aWCHuvjodGciE=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    ninja
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    wayland
+    wayland-protocols
+    cairo
+  ];
+
+  mesonBuildType = "release";
+
+  env.NIX_CFLAGS_COMPILE = toString [
+    "-Wno-error=maybe-uninitialized"
+  ];
+
+  meta = {
+    description = "Likely the fastest, least user-friendly, and worst named Wayland wallpaper utility possible.";
+    homepage = "https://git.sr.ht/~kennylevinsen/wayllpaper";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    mainProgram = "wayllpaper";
+  };
+})
---
+++ packages/wleird.nix
@@ -0,0 +1,51 @@
+{
+  stdenv,
+  lib,
+  fetchgit,
+  pkg-config,
+  meson,
+  ninja,
+  wayland,
+  wayland-scanner,
+  wayland-protocols,
+  cairo,
+  libgbm,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wleird";
+  version = "git";
+
+  src = fetchgit {
+    url = "https://gitlab.freedesktop.org/emersion/wleird.git";
+    rev = "73439834b73d2a5ea68ce6725f625e0e08d96279";
+    hash = "sha256-2rWSBtrw61l7ofpnxFj+zRvoHUmuwBlslbhhGNl5Y08=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    ninja
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    wayland
+    wayland-protocols
+    cairo
+    libgbm
+  ];
+
+  mesonBuildType = "release";
+
+  env.NIX_CFLAGS_COMPILE = toString [
+    "-Wno-error"
+  ];
+
+  meta = {
+    description = "A collection a Wayland clients doing weird things, for compositor testing";
+    homepage = "https://gitlab.freedesktop.org/emersion/wleird";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+  };
+})
