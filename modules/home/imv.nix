5264566030caee6c6ab177ace24a9e590d2e1d14
PunchlY <punchly9lin@gmail.com>
2026-04-01 16:14:23 +0800

    feat(home): add imv image viewer configuration

---
+++ modules/home/imv.nix
@@ -0,0 +1,32 @@
+{
+  nixosConfig,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (nixosConfig.theme) font colors opacity;
+in
+{
+  config = lib.mkIf config.programs.imv.enable {
+    xdg.mimeApps.defaultApplications."image/*" = "imv.desktop";
+    xdg.mimeApps.defaultApplicationPackages = [
+      config.programs.imv.package
+    ];
+    programs.imv = {
+      settings.options = with colors; {
+        background = surface.hex_stripped;
+        overlay = true;
+        overlay_text_color = on_surface.hex_stripped;
+        overlay_font = "${font.name}:${toString font.size}";
+        overlay_background_color = surface.hex_stripped;
+        overlay_background_alpha = lib.fixedWidthString 2 "0" (
+          lib.toHexString (builtins.ceil (opacity * 255))
+        );
+        overlay_text = ''$imv_current_index/$imv_file_count — $(basename -- "$imv_current_file")  [$imv_scale%]'';
+        title_text = ''$(basename -- "$imv_current_file")'';
+      };
+    };
+  };
+}
