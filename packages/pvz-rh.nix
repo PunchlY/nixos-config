68166a48fc6060148d2c1e1c9b1b79d6b1db49bd
PunchlY <punchly9lin@gmail.com>
2026-04-06 18:30:50 +0800

    update(pvz-rh): upgrade to v3.5.0

--- packages/pvz-rh.nix
+++ packages/pvz-rh.nix
@@ -5,10 +5,11 @@
 }:
 stdenv.mkDerivation {
   name = "PlantsVsZombiesRH";
+  version = "v3.5.0";
   src = requireFile {
     name = "PlantsVsZombiesRH.zip";
     url = "https://wiki.biligame.com/pvzrh";
-    hash = "sha256-5Orpgas80710KZzYayS8qkSAipCCmSuqxOSaZduyFOI=";
+    hash = "sha256-Wbbat6fFetlf4Lm2jbPRdvC1VCRJxIEbjCayH/Yinx0=";
   };
 
   nativeBuildInputs = [
@@ -18,6 +19,6 @@ stdenv.mkDerivation {
   buildCommand = ''
     mkdir -p $out
     unzip $src -d $out
-    mv "$out/#U690d#U7269#U5927#U6218#U50f5#U5c38#U878d#U5408#U72483.2/"* "$out"
+    mv "$out/#U690d#U7269#U5927#U6218#U50f5#U5c38#U878d#U5408#U72483.5/"* "$out"
   '';
 }
