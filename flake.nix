fdea25ea4786303477e6978513ec4e50550e6387
PunchlY <punchly9lin@gmail.com>
2026-04-12 01:47:49 +0800

    refactor(nix): move nix config from flake.nix to settings module

--- flake.nix
+++ flake.nix
@@ -40,16 +40,6 @@
     };
   };
 
-  nixConfig.substituters = [
-    "https://mirrors.ustc.edu.cn/nix-channels/store"
-    "https://nix-community.cachix.org"
-    "https://cache.nixos.org"
-  ];
-  nixConfig.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  ];
-
   outputs = inputs: let
     inherit (inputs.nixpkgs) lib;
     eachSystem = lib.genAttrs (import inputs.systems);
