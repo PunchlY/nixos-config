010ec63687ce1d950ddf015d1995f0d04026411a
PunchlY <punchly9lin@gmail.com>
2026-05-04 22:52:57 +0800

    chore: update flake dependencies and disable fprintd on GPD Win Max 2

--- modules/profiles/winmax2/gpd-win-max-2-2022.nix
+++ modules/profiles/winmax2/gpd-win-max-2-2022.nix
@@ -19,34 +19,34 @@
 
     hardware.sensor.iio.enable = true;
 
-    services.fprintd.enable = true;
-    nixpkgs.overlays = [
-      (final: prev: {
-        libfprint = prev.libfprint.overrideAttrs (oldAttrs: {
-          version = "git";
-          src = final.fetchFromGitHub {
-            owner = "archeYR";
-            repo = "libfprint-CS9711";
-            rev = "02b285c9703c38d308fbe47a3c566ef1e7f883ca";
-            sha256 = "sha256-QGrBNqbRNqLZIURI66xkenlQamNW+DQU4WS+CLN4zM8=";
-          };
-          nativeBuildInputs =
-            oldAttrs.nativeBuildInputs
-            ++ [
-              final.opencv
-              final.cmake
-              final.doctest
-            ];
-          buildInputs =
-            oldAttrs.buildInputs
-            ++ [
-              final.nss
-            ];
-        });
-      })
-    ];
+    # services.fprintd.enable = true;
+    # nixpkgs.overlays = [
+    #   (final: prev: {
+    #     libfprint = prev.libfprint.overrideAttrs (oldAttrs: {
+    #       version = "git";
+    #       src = final.fetchFromGitHub {
+    #         owner = "archeYR";
+    #         repo = "libfprint-CS9711";
+    #         rev = "02b285c9703c38d308fbe47a3c566ef1e7f883ca";
+    #         sha256 = "sha256-QGrBNqbRNqLZIURI66xkenlQamNW+DQU4WS+CLN4zM8=";
+    #       };
+    #       nativeBuildInputs =
+    #         oldAttrs.nativeBuildInputs
+    #         ++ [
+    #           final.opencv
+    #           final.cmake
+    #           final.doctest
+    #         ];
+    #       buildInputs =
+    #         oldAttrs.buildInputs
+    #         ++ [
+    #           final.nss
+    #         ];
+    #     });
+    #   })
+    # ];
 
-    security.pam.services.greetd.fprintAuth = false;
+    # security.pam.services.greetd.fprintAuth = false;
 
     services.udev.extraRules = ''
       SUBSYSTEM=="power_supply", KERNEL=="ADP1", ATTR{online}=="1", RUN+="${pkgs.ryzenadj}/bin/ryzenadj ${
