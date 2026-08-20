1a02dd4a34714160308be7e33704d4f9f3d72848
PunchlY <punchly9lin@gmail.com>
2026-08-20 17:28:23 +0800

    refactor(packages): 简化 nstore 脚本并更新植物大战僵尸融合版

--- packages/custom-scripts/scripts/nstore.sh
+++ packages/custom-scripts/scripts/nstore.sh
@@ -3,18 +3,16 @@ set -euo pipefail
 
 [ -z "$1" ] && exit
 
-basename="${2:-$(@coreutils@/bin/basename "$1")}"
+store="$(@nix@/bin/nix-store --add-fixed sha256 "$1")"
 
-store="$(nix store add "$1" --name "$basename")"
+hash="$(@nix@/bin/nix hash file --type sha256 --sri "$1")"
 
-hash="$(nix-hash --to-sri --type sha256 "$(@nix@/bin/nix-store --query --hash "$store")")"
-
-name="$(@coreutils@/bin/basename "$store" | @gnused@/bin/sed 's/^[a-z0-9]\{32\}-//')"
+name="$(@coreutils@/bin/basename "$1")"
 
 @nix@/bin/nix-store --add-root "$XDG_DATA_HOME/nstore/$name" --indirect --realise $store
 
 echo "requireFile {
   name = \"$name\";
-  url = "...";
+  url = \"...\";
   hash = \"$hash\";
 }"
--- packages/pvz-rh.nix
+++ packages/pvz-rh.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation {
   pname = "PlantsVsZombiesRH";
-  version = "3.6.1";
+  version = "3.8.1";
   src = requireFile {
     name = "PlantsVsZombiesRH.zip";
     url = "https://wiki.biligame.com/pvzrh";
-    hash = "sha256-duhtgXHrIuMATlxpUroWDsfidaaOrWWEIM6B6kVauPM=";
+    hash = "sha256-PYIj1vhQmmvyuOOzMxpgyl3XGXCyctK1daCT55p5C3M=";
   };
 
   nativeBuildInputs = [
@@ -19,6 +19,6 @@ stdenv.mkDerivation {
   buildCommand = ''
     mkdir -p $out
     unzip $src -d .
-    mv ./"#U690d#U7269#U5927#U6218#U50f5#U5c38#U878d#U5408#U72483.6.1/"* "$out"
+    mv ./"#U690d#U7269#U5927#U6218#U50f5#U5c38#U878d#U5408#U72483.9/"* "$out"
   '';
 }
